d26b9eb4bd906a61a3a86044c50e971c282223d3
PC <ngcribeiro96@gmail.com>
2021-12-22 23:13:42 -0300

    Heranca na Pratica

--- Programando com Orientacao a Objeto com C#/ExemploPOO/Program.cs
+++ Programando com Orientacao a Objeto com C#/ExemploPOO/Program.cs
@@ -7,18 +7,29 @@ namespace ExemploPOO
     {
         static void Main(string[] args)
         {
+
+            Professor p1 = new Professor();
+            p1.Salario = 2000;
+            p1.Nome = "Bob";
+            p1.Idade = 20;
+            p1.Documento = "123456";
+            
+            p1.Apresentar();
+
+//===========================================================================
             // Valores Validos
-            Retangulo r = new Retangulo();
-            r.DefinirMedidas(30,30);
+            // Retangulo r = new Retangulo();
+            // r.DefinirMedidas(30,30);
             
-            System.Console.WriteLine($"Area: {r.ObterArea()}");
+            // System.Console.WriteLine($"Area: {r.ObterArea()}");
 
-            // Valores Invalidos
-            Retangulo r2 = new Retangulo();
-            r2.DefinirMedidas(0,0);
+            // // Valores Invalidos
+            // Retangulo r2 = new Retangulo();
+            // r2.DefinirMedidas(0,0);
             
-            System.Console.WriteLine($"Area: {r2.ObterArea()}");
+            // System.Console.WriteLine($"Area: {r2.ObterArea()}");
 
+//===========================================================================
 
             // Pessoa p1 = new Pessoa();
             // p1.Nome = "Bob";
