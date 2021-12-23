434839df990d494f7af58bd4c84a514ae57f1824
PC <ngcribeiro96@gmail.com>
2021-12-23 20:37:12 -0300

    Polimorfismo tempo de compilacao

--- Programando com Orientacao a Objeto com C#/ExemploPOO/Program.cs
+++ Programando com Orientacao a Objeto com C#/ExemploPOO/Program.cs
@@ -8,19 +8,24 @@ namespace ExemploPOO
         static void Main(string[] args)
         {
 
-            Aluno p1 = new Aluno();
-            p1.Nome = "Bob";
-            p1.Idade = 20;
-            p1.Documento = "123456";
-            p1.Nota = 10;
-            p1.Apresentar();
+            Calculadora cal = new Calculadora();
+            System.Console.WriteLine("Resultado da primeira soma: " +cal.Somar(10,10));
+            System.Console.WriteLine("Resultado da segunda soma: " +cal.Somar(10,10,10));
+
+//===========================================================================
+            // Aluno p1 = new Aluno();
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+            // p1.Documento = "123456";
+            // p1.Nota = 10;
+            // p1.Apresentar();
             
-            Professor p2 = new Professor();
-            p2.Nome = "Leo";
-            p2.Idade = 20;
-            p2.Documento = "123456";
-            p2.Salario = 1000;
-            p2.Apresentar();
+            // Professor p2 = new Professor();
+            // p2.Nome = "Leo";
+            // p2.Idade = 20;
+            // p2.Documento = "123456";
+            // p2.Salario = 1000;
+            // p2.Apresentar();
 
 //===========================================================================
             // Valores Validos
