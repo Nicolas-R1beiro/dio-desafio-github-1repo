6d67e9ed29a12eca4e4c9aa86800612f3f223478
PC <ngcribeiro96@gmail.com>
2021-12-23 20:29:08 -0300

    Polimorfismo em tempo de execucao

--- Programando com Orientacao a Objeto com C#/ExemploPOO/Program.cs
+++ Programando com Orientacao a Objeto com C#/ExemploPOO/Program.cs
@@ -8,13 +8,19 @@ namespace ExemploPOO
         static void Main(string[] args)
         {
 
-            Professor p1 = new Professor();
-            p1.Salario = 2000;
+            Aluno p1 = new Aluno();
             p1.Nome = "Bob";
             p1.Idade = 20;
             p1.Documento = "123456";
-            
+            p1.Nota = 10;
             p1.Apresentar();
+            
+            Professor p2 = new Professor();
+            p2.Nome = "Leo";
+            p2.Idade = 20;
+            p2.Documento = "123456";
+            p2.Salario = 1000;
+            p2.Apresentar();
 
 //===========================================================================
             // Valores Validos
