0a07155b043c7548aa49b1909628221ba64a2288
PC <ngcribeiro96@gmail.com>
2022-01-05 23:19:01 -0300

    Verificando um elemento existente

--- Colecoes e LINQ com .NET/Colecoes/Program.cs
+++ Colecoes e LINQ com .NET/Colecoes/Program.cs
@@ -1,24 +1,60 @@
+using Colecoes.Helper;
+
 namespace Colecoes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            int[,] matriz = new int[4,2]
+            OperacoesArray op = new OperacoesArray();
+
+            int[] array = new int[5] { 6, 3, 8, 1, 9};
+            int[] arrayCopia = new int [10];
+
+            int valorProcurado = 10;
+            bool existe = op.Existe(array, valorProcurado);
+
+            if(existe)
             {
-                { 8, 8 },
-                { 10, 20 },
-                { 50, 100 },
-                { 90, 200}
-            };
-            
-            for (int i = 0; i < matriz.GetLength(0); i++)
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)
-                {
-                    System.Console.WriteLine(matriz[i, j]);
-                }
+                System.Console.WriteLine("Encontrei o valor: {0}", valorProcurado);
             }
+            else
+            {
+                System.Console.WriteLine("Nao encontrei o valor: {0}", valorProcurado);
+            }
+
+//========================================================================  
+            //System.Console.WriteLine("Array Original:");
+            //op.ImprimirArray(array);
+
+            //op.OrdernarBubbleSort(ref array);
+            //System.Console.WriteLine("Array Ordenado:");
+            //op.ImprimirArray(array);
+
+//========================================================================  
+            // System.Console.WriteLine("Array antes da copia:");
+            // op.ImprimirArray(arrayCopia);
+
+            // op.Copiar(ref array, ref arrayCopia);
+            // System.Console.WriteLine("Array apos a copia:");
+            // op.ImprimirArray(arrayCopia);
+
+//========================================================================            
+            // int[,] matriz = new int[4,2]
+            // {
+            //     { 8, 8 },
+            //     { 10, 20 },
+            //     { 50, 100 },
+            //     { 90, 200}
+            // };
+            
+            // for (int i = 0; i < matriz.GetLength(0); i++)
+            // {
+            //     for (int j = 0; j < matriz.GetLength(1); j++)
+            //     {
+            //         System.Console.WriteLine(matriz[i, j]);
+            //     }
+            // }
 
 //========================================================================
             // int[] arrayInteiros = new int[3];
