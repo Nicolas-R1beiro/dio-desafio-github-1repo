5e8aab76df777df1ef7529a3ef214c4d997c2ad7
PC <ngcribeiro96@gmail.com>
2022-01-21 23:47:59 -0300

    Reference Types na pratica

---
+++ Trabalhando com Tipos de Referencia e Valor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    static int Adicionar20(int num)
+    {
+        return num + 20;
+    }
+
+    static void TrocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
+        public static void Main()
+    {
+        // int a = 2;
+        // a = Adicionar20(a);
+
+        // System.Console.WriteLine($"O valor da variavel a é: {a} ");
+
+        Pessoa p1 = new Pessoa();
+        
+        p1.Nome = "Nicolas";
+        p1.Idade = 18;
+        p1.Documento = "1234";
+
+        TrocarNome(p1,"José");
+
+        System.Console.WriteLine($"O novo nome é: {p1.Nome}");
+    }
+}
